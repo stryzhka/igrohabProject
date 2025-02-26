b9c0c6484ef33a7a0ed3d6645f52a46629d947d1
stryzhka <voditel.vlad@yandex.ru>
2025-02-26 08:58:14 +0300

    wheel physics with jump

--- Assets/Scripts/Movement/PlayerMovement.cs
+++ Assets/Scripts/Movement/PlayerMovement.cs
@@ -2,32 +2,50 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] private float _Gravity;
-    [SerializeField] private float _PlayerVelocity;
 
     [SerializeField] private float _MovementSpeed;
 
     [SerializeField] private float _JumpThrust;
 
     [SerializeField] private Rigidbody2D _Rigidbody;
-    private float position = 0f;
+
+    [SerializeField] private Transform _PlayerCollider;
+
+    [SerializeField] private LayerMask _Ground;
+
+    private float _lastY;
+    public bool _onGround;
     void Start()
     {
-        
+
     }
 
     
     void Update()
     {
+        _onGround = Physics2D.OverlapCircle(_PlayerCollider.transform.position, 1f, _Ground);
         Move();
     }
 
     void Move()
     {
-        if (Input.GetButtonDown("Jump"))
+        
+        if (_onGround)
         {
-            Debug.Log("dfsf");
-            _Rigidbody.AddForce(transform.up * _JumpThrust, ForceMode2D.Impulse);
+            float xMove = Input.GetAxis("Horizontal");
+            if (xMove != 0)
+            {
+                var impulse = (-xMove * _MovementSpeed * Mathf.Deg2Rad ) * _Rigidbody.inertia; 
+                _Rigidbody.AddTorque(impulse, ForceMode2D.Impulse);
+            }
+            if (Input.GetButtonDown("Jump"))
+            {
+                Debug.Log("dfsf");
+                _Rigidbody.AddForce(new Vector2(0, 1) * _JumpThrust, ForceMode2D.Impulse);
+                
+            }
         }
+        
+        
     }
 }
