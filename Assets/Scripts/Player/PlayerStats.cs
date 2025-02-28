9e29429736dee9f1dad676faa1f96069eca7846c
stryzhka <voditel.vlad@yandex.ru>
2025-02-28 12:01:13 +0300

    initial commit of new branch

--- Assets/Scripts/Player/PlayerStats.cs
+++ Assets/Scripts/Player/PlayerStats.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class PlayerStats : MonoBehaviour
 {
     [SerializeField] private int HealthPoints;
-    
+    [SerializeField] private int Lives;
     void Start()
     {
         
